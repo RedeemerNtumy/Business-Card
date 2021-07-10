ea2c1fc256d7bc9a046f203f7392af8488243915
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-07-10 21:05:19 +0000

    Made improvements in my Flutter Business Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Peace 'n' Love Natural Mineral Water"),
-        backgroundColor: Colors.lightBlue[600],
-      ),
-      body: Center(
-        child: Image(
-          image: AssetImage('assets/images/dog.0.jpg'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Center(child: Text("ABOUT ME")),
+        ),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.only(left: 20, top: 50),
+            padding: EdgeInsets.only(left: 25),
+            height: 100,
+            width: 200,
+            color: Colors.teal[400],
+            child: Text("My name is Redeemer Ntumy"),
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
