1c301f31682a88e9f2055ce51e47dfeef42765a6
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-07-10 21:35:15 +0000

    Changes still being made

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,40 @@ class MyApp extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: Container(
-            margin: EdgeInsets.only(left: 20, top: 50),
-            padding: EdgeInsets.only(left: 25),
-            height: 100,
-            width: 200,
-            color: Colors.teal[400],
-            child: Text("My name is Redeemer Ntumy"),
-          ),
-        ),
+            child: Column(
+          children: <Widget>[
+            Container(
+                margin: EdgeInsets.only(left: 20, top: 50),
+                padding: EdgeInsets.only(left: 25),
+                height: 100,
+                width: 200,
+                color: Colors.teal[400],
+                child: Text("Using Container for the first time")),
+            Container(
+                margin: EdgeInsets.only(left: 20, top: 10),
+                padding: EdgeInsets.only(left: 25),
+                height: 100,
+                width: 200,
+                color: Colors.blue[400],
+                child: Text("Using Container for the first time")),
+            Row(
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.only(left: 20),
+                    height: 100,
+                    width: 161,
+                    color: Colors.red,
+                    child: Text("yes")),
+                Container(
+                    margin: EdgeInsets.only(left: 10, right: 20),
+                    height: 100,
+                    width: 161,
+                    color: Colors.orange,
+                    child: Text("no")),
+              ],
+            )
+          ],
+        )),
       ),
     );
   }
